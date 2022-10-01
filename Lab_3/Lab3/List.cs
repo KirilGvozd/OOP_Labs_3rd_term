9845b95e334a15843b9a023e4c782087f7495c27
Kirill <kirillgvozd0@gmail.com>
2022-10-02 00:01:31 +0300

    Created third lab, started working on it

---
+++ Lab_3/Lab3/List.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab3
+{
+    public class List
+    {
+        public class Node
+        {
+            public int data { get; set; }
+            public Node Next { get; set; }
+        }
+        
+        
+        
+        public static void Main()
+        {
+            
+        }
+    }
+}
