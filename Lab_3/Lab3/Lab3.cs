08b6a2eed8d6b2dbb1d3d6b3f5a8f85735c7908b
Kirill <kirillgvozd0@gmail.com>
2022-10-07 22:49:18 +0300

    Made first task in the third lab

---
+++ Lab_3/Lab3/Lab3.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab3;
+
+public class Lab3
+{
+    public static void Main()
+    {
+        Stack stack1 = new Stack(10);
+        stack1.Push(200);
+        stack1.Push(300);
+        Console.WriteLine("Element from the top of stack: " + stack1.TopElement());
+        stack1.Pop();
+        Console.WriteLine("Element from the top of stack: " + stack1.TopElement());
+    }
+}
---
+++ Lab_3/Lab3/Stack.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+
+namespace Lab3
+{
+    class Stack
+    {
+        public class Production
+        {
+            
+        }
+        
+        public int[] MyStack;
+        public int Counter;
+        public int StackCapacity;
+
+        public Stack(int n)
+        {
+            MyStack = new int[n];
+            Counter = 0;
+            StackCapacity = n;
+        }
+
+        public void Push(int item)
+        {
+            if (!IsStackFull())
+            {
+                MyStack[Counter++] = item;
+            }
+            else
+                Console.WriteLine("Stack is Full");
+        }
+
+        public bool IsStackFull()
+        {
+            if (Counter == StackCapacity)
+                return true;
+            else
+                return false;
+        }
+
+        public object Pop()
+        {
+            if (MyStack.Length != 0)
+                return MyStack[--Counter];
+            return -1;
+            Console.WriteLine("Stack is empty");
+        }
+
+        public object TopElement()
+        {
+            if (MyStack.Length != 0)
+                return MyStack[Counter - 1];
+            return 0;
+        }
+        
+        public static Stack operator *(Stack givenStack, int data)
+        {
+            givenStack.Push(data);
+            return givenStack;
+        }
+
+        public static Stack operator / (Stack givenStack, int data)
+        {
+            givenStack.Pop();
+            return givenStack;
+        }
+
+        public static bool operator true (Stack givenStack)
+        {
+            for (int i = 0; i < givenStack.StackCapacity; i++)
+            {
+                if (givenStack.MyStack[i] < 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator false(Stack givenStack)
+        {
+            for (int i = 0; i < givenStack.StackCapacity; i++)
+            {
+                if (givenStack.MyStack[i] < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool operator ==(Stack firstStack, Stack secondStack)
+        {
+            int counter = 0;
+            
+            if (firstStack.StackCapacity == secondStack.StackCapacity)
+            {
+                for (int i = 0; i < firstStack.StackCapacity; i++)
+                {
+                    if (firstStack.MyStack[i] != secondStack.MyStack[i])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            if (counter == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Stack firstStack, Stack secondStack)
+        {
+            int counter = 0;
+            
+            if (firstStack.StackCapacity == secondStack.StackCapacity)
+            {
+                for (int i = 0; i < firstStack.StackCapacity; i++)
+                {
+                    if (firstStack.MyStack[i] != secondStack.MyStack[i])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            else
+            {
+                return true;
+            }
+
+            if (counter == 0)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+    }    
+}
