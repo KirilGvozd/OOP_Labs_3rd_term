14a2dbd788870599a9b1427fc568d741c90e0680
Kirill <kirillgvozd0@gmail.com>
2022-10-15 15:41:30 +0300

    Added answers for fourth lab

--- Lab_4/Lab4/Person.cs
+++ Lab_4/Lab4/Person.cs
@@ -4,4 +4,9 @@ public class Person
 {
     protected string? NameOfThePerson;
     protected string? SurnameOfThePerson;
+
+    public Person()
+    {
+        Console.WriteLine("Constructor.");
+    }
 }
