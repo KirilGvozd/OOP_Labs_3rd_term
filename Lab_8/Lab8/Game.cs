0505bb28c301df3436e0d3ad159fd59eca1c7487
Kirill <kirillgvozd0@gmail.com>
2022-11-04 10:47:42 +0300

    Started work on the eighth lab

---
+++ Lab_8/Lab8/Game.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab8;
+
+public class Game
+{
+    private static int Health { get; set; }
+
+    public Game(int health)
+    {
+        Health = health;
+    }
+
+    public static void Attack(int damage)
+    {
+        Health -= damage;
+    }
+
+    public static void Healing(int addedHealth)
+    {
+        Health += addedHealth;
+    }
+}
---
+++ Lab_8/Lab8/Lab8.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab8;
+
+public delegate void FirstDelegate(int healthPoints);
+
+public static class Lab8
+{
+    public static void Main()
+    {
+        Game firstPlayer = new Game(200);
+        Game secondPlayer = new Game(300);
+        Game thirdPlayer = new Game(400);
+        FirstDelegate attack;
+        attack = new FirstDelegate(Game.Attack);
+        
+    }
+}
